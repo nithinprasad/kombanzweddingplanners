a38413824bfb8b2ff4dbca7959bc551719d57a6a
Nithin Prasad <nithinprasad549@yahoo.com>
2024-12-15 15:23:36 +0530

    version updates

---
+++ src/app/section/contact/contact.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroImageService } from 'src/app/service/hero-image.service';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor(private imageService : HeroImageService ) { }
+
+  images: string[] = [];
+
+  ngOnInit(): void {
+    this.images = this.imageService.getContactImages()
+  }
+
+}
